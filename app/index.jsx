f086f1dca985034ecdf5207789d33dfdd3526694
Regameroh87 <gamero.rodrigo@gmail.com>
2026-02-20 17:46:14 -0300

    feat: Wrap main view in SafeAreaProvider for better layout handling

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
+import { SafeAreaProvider, sa } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold text-red-500">INDEX</Text>
-    </View>
+    <SafeAreaProvider>
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-2xl font-bold text-red-500">INDEX</Text>
+      </View>
+    </SafeAreaProvider>
   );
 }
