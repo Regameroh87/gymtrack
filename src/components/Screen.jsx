a7efb2710e3271779f5c487fcca0884da0469e8a
Regameroh87 <gamero.rodrigo@gmail.com>
2026-02-21 12:57:35 -0300

    feat: Add padding to Screen component for improved layout

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,7 +1,7 @@
 import { View } from "react-native";
 
 const Screen = ({ children }) => {
-  return <View className="flex-1 items-center">{children}</View>;
+  return <View className="flex-1 items-center pt-4">{children}</View>;
 };
 
 export default Screen;
