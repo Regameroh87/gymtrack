1337828f2df147b26980c64649280d2ee01d9520
Regameroh87 <gamero.rodrigo@gmail.com>
2026-02-23 11:43:54 -0300

    refactor: Simplify login process by removing unused code and enhancing email sending logic

--- app/login.jsx
+++ app/login.jsx
@@ -5,40 +5,26 @@ import { useUser } from "../src/lib/authContext";
 import { useRouter } from "expo-router";
 
 export default function Login() {
-  // Dentro de tu AuthProvider
-  /*   const logIn = async (email) => {
+  const enviarCodigo = async (email) => {
     const { error } = await supabase.auth.signInWithOtp({
       email: email,
       options: {
-        // Esto asegura que si el usuario no existe en tu "lista blanca",
-        // Supabase no lo cree automáticamente (si tienes el registro cerrado)
+        // Importante: Al poner false, si el mail no existe en tu lista,
+        // Supabase no enviará nada ni creará un usuario nuevo.
         shouldCreateUser: false,
+        // Puedes redirigir a una web después, pero para App móvil
+        // usualmente el usuario solo espera el código.
+        emailRedirectTo: "tuapp://login",
       },
     });
 
     if (error) {
-      console.error("Error al enviar código:", error.message);
+      console.error("Error al enviar:", error.message);
       throw error;
     }
 
-    // Aquí no seteamos isLoggedIn(true) todavía,
-    // porque falta que el usuario ingrese el código que le llegó.
+    alert("Revisa tu bandeja de entrada, te enviamos un código.");
   };
-
-  // Necesitaremos una segunda función para verificar el código
-  const verifyCode = async (email, token) => {
-    const { error } = await supabase.auth.verifyOtp({
-      email,
-      token,
-      type: "magiclink", // o 'signup' / 'token_hash' según configures
-    });
-
-    if (!error) {
-      setIsLoggedIn(true); // ¡Ahora sí está dentro!
-    } else {
-      throw error;
-    }
-  }; */
   const { logIn } = useUser();
   const router = useRouter();
 
@@ -48,8 +34,9 @@ export default function Login() {
       <Button
         title="Log In"
         onPress={async () => {
-          logIn();
-          router.replace("/");
+          /*  logIn();
+          router.replace("/"); */
+          enviarCodigo("gamero.rodrigo@gmail.com");
         }}
       />
     </Screen>
