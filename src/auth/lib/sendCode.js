588dcb7e289b8199348bbe2840e09b8d4c6c1454
Regameroh87 <gamero.rodrigo@gmail.com>
2026-03-14 17:05:30 -0300

    feat: Add user existence validation by checking the profiles table before sending an authentication code.

--- src/auth/lib/sendCode.js
+++ src/auth/lib/sendCode.js
@@ -1,11 +1,24 @@
 import { supabase } from "../../database/supabase";
 const sendCodeVerify = async (email) => {
-  const { data, error } = await supabase.auth.signInWithOtp({
-    email: email,
-    options: {
-      shouldCreateUser: false,
-    },
-  });
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("email", email);
+  if (data) {
+    const { data: dataOtp, error: errorOtp } =
+      await supabase.auth.signInWithOtp({
+        email: email,
+        options: {
+          shouldCreateUser: false,
+        },
+      });
+    if (errorOtp) {
+      console.error("Error al enviar:", errorOtp.message);
+      throw errorOtp;
+    }
+    return dataOtp;
+  }
+
   if (error) {
     console.error("Error al enviar:", error.message);
     throw error;
