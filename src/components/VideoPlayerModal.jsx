80038bc6d6782e5343bc11a78f26f3c0f42b6cb3
Regameroh87 <gamero.rodrigo@gmail.com>
2026-03-21 22:26:48 -0300

    refactor: remove unnecessary Screen component wrapping the video player modal.

--- src/components/VideoPlayerModal.jsx
+++ src/components/VideoPlayerModal.jsx
@@ -39,43 +39,41 @@ export default function VideoPlayerModal({ visible, onClose, videoUrl }) {
   if (!videoUrl) return null;
 
   return (
-    <Screen>
-      <Modal
-        visible={visible}
-        animationType="slide"
-        transparent={false}
-        onRequestClose={onClose}
-      >
-        <View className=" absolute top-20 right-4 z-20">
-          <Pressable
-            onPress={onClose}
-            className=" bg-ui-secondary-dark/40 p-2 rounded-full"
-          >
-            <X color="#fff" size={24} />
-          </Pressable>
-        </View>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            backgroundColor: "black",
-          }}
+    <Modal
+      visible={visible}
+      animationType="slide"
+      transparent={false}
+      onRequestClose={onClose}
+    >
+      <View className=" absolute top-20 right-4 z-20">
+        <Pressable
+          onPress={onClose}
+          className=" bg-ui-secondary-dark/40 p-2 rounded-full"
         >
-          {isYouTube(videoUrl) ? (
-            <View style={{ width: "100%", height: width * (9 / 16) }}>
-              <YoutubePlayer
-                height={width * (9 / 16)}
-                play={true}
-                videoId={getYouTubeId(videoUrl)}
-                forceAndroidAutoplay={true}
-                webViewStyle={{ opacity: 0.99 }} // Hack crucial para Android WebView
-              />
-            </View>
-          ) : (
-            <CloudinaryPlayer url={videoUrl} />
-          )}
-        </View>
-      </Modal>
-    </Screen>
+          <X color="#fff" size={24} />
+        </Pressable>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          backgroundColor: "black",
+        }}
+      >
+        {isYouTube(videoUrl) ? (
+          <View style={{ width: "100%", height: width * (9 / 16) }}>
+            <YoutubePlayer
+              height={width * (9 / 16)}
+              play={true}
+              videoId={getYouTubeId(videoUrl)}
+              forceAndroidAutoplay={true}
+              webViewStyle={{ opacity: 0.99 }} // Hack crucial para Android WebView
+            />
+          </View>
+        ) : (
+          <CloudinaryPlayer url={videoUrl} />
+        )}
+      </View>
+    </Modal>
   );
 }
