2e5b1301d5f84a8f49368487d040d390074f97d0
Regameroh87 <gamero.rodrigo@gmail.com>
2026-03-24 15:40:16 -0300

    feat: Add YouTube video playback support using `react-native-youtube-iframe`.

--- src/components/videos/PreviewVideo.jsx
+++ src/components/videos/PreviewVideo.jsx
@@ -3,10 +3,13 @@ import { useVideoPlayer, VideoView } from "expo-video";
 import { LinearGradient } from "expo-linear-gradient";
 import { useColorScheme } from "nativewind";
 import { gradient } from "../../theme/colors";
+import YoutubePlayer from "react-native-youtube-iframe";
 
 export default function PreviewVideo({ videoUrl, children }) {
   const { colorScheme } = useColorScheme();
+  const isYoutube = videoUrl.includes("youtube.com");
   console.log(videoUrl);
+
   const isDark = colorScheme === "dark";
   const player = useVideoPlayer(videoUrl, (p) => {
     p.loop = true;
@@ -15,7 +18,32 @@ export default function PreviewVideo({ videoUrl, children }) {
 
   return (
     <View className="flex flex-row items-center justify-center h-52">
-      {videoUrl ? (
+      {isYoutube ? (
+        <View
+          className="relative w-full h-full"
+          style={{ borderRadius: 16, overflow: "hidden" }}
+        >
+          <YoutubePlayer
+            videoId={videoUrl.split("v=")[1]}
+            height={"100%"}
+            width={"100%"}
+          />
+          <LinearGradient
+            colors={["rgba(12, 10, 29, 0.15)", "rgba(12, 10, 29, 0.65)"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              height: "100%",
+              zIndex: 10,
+            }}
+            pointerEvents="none"
+          />
+        </View>
+      ) : videoUrl ? (
         <View
           className="relative w-full h-full"
           style={{ borderRadius: 16, overflow: "hidden" }}
