3e4ca543817435561ee2f50c61d1741637eea59a
Regameroh87 <gamero.rodrigo@gmail.com>
2026-04-01 22:41:47 -0300

    feat: add uuid generation for exercise IDs and include created_at and updated_at timestamps in exercises_base schema

--- src/database/schemas.js
+++ src/database/schemas.js
@@ -1,7 +1,9 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
-
+import { v4 as uuidv4 } from "uuid";
 export const exercisesBase = sqliteTable("exercises_base", {
-  id: text("id").primaryKey(),
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => uuidv4()),
   name: text("name").notNull(),
   category: text("category").notNull(),
   muscle_group: text("muscle_group").notNull(),
@@ -11,4 +13,6 @@ export const exercisesBase = sqliteTable("exercises_base", {
   image_public_id: text("image_public_id").notNull(),
   instructions: text("instructions").notNull(),
   is_unilateral: text("is_unilateral").notNull(),
+  created_at: text("created_at").$defaultFn(() => new Date().toISOString()),
+  updated_at: text("updated_at").$defaultFn(() => new Date().toISOString()),
 });
