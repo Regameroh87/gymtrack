0c37b34548fa9e973a143a978ca75588434634eb
Regameroh87 <gamero.rodrigo@gmail.com>
2026-04-03 09:31:45 -0300

    feat: add second database migration for exercises_base table

---
+++ src/database/migrations/migrations.js
@@ -0,0 +1,14 @@
+import journal from './meta/_journal.json';
+import m0000 from './0000_broken_dark_phoenix.sql';
+import m0001 from './0001_square_shinko_yamashiro.sql';
+import m0002 from './0002_steep_james_howlett.sql';
+
+  export default {
+    journal,
+    migrations: {
+      m0000,
+      m0001,
+m0002
+    }
+  }
+  
