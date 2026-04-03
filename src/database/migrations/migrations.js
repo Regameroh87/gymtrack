c12d0de10b61a0266d74d6cae011946b2c3e6c2d
Regameroh87 <gamero.rodrigo@gmail.com>
2026-04-03 09:34:28 -0300

    refactor: map migration objects to explicit string keys in migrations configuration

--- src/database/migrations/migrations.js
+++ src/database/migrations/migrations.js
@@ -3,12 +3,12 @@ import m0000 from './0000_broken_dark_phoenix.sql';
 import m0001 from './0001_square_shinko_yamashiro.sql';
 import m0002 from './0002_steep_james_howlett.sql';
 
-  export default {
-    journal,
-    migrations: {
-      m0000,
-      m0001,
-m0002
-    }
+export default {
+  journal,
+  migrations: {
+    '0000_broken_dark_phoenix': m0000,
+    '0001_square_shinko_yamashiro': m0001,
+    '0002_steep_james_howlett': m0002,
   }
+}
   
