b26c5cb45ea5ccfd43a916c0ef052dae96dc239b
Regameroh87 <gamero.rodrigo@gmail.com>
2026-04-08 14:18:23 -0300

    feat: update app scheme and migrate equipment and exercise media fields to support local and cloud storage

--- src/database/migrations/0003_luxuriant_gauntlet.sql
+++ src/database/migrations/0003_luxuriant_gauntlet.sql
@@ -1,9 +1,9 @@
 ALTER TABLE `equipment` ADD `cloudinary_image_public_id` text;--> statement-breakpoint
-ALTER TABLE `equipment` ADD `local_image_uri` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `equipment` ADD `local_image_uri` text;--> statement-breakpoint
 ALTER TABLE `equipment` DROP COLUMN `image_public_id`;--> statement-breakpoint
 ALTER TABLE `exercises_base` ADD `cloudinary_video_public_id` text;--> statement-breakpoint
 ALTER TABLE `exercises_base` ADD `cloudinary_image_public_id` text;--> statement-breakpoint
-ALTER TABLE `exercises_base` ADD `local_image_uri` text NOT NULL;--> statement-breakpoint
-ALTER TABLE `exercises_base` ADD `local_video_uri` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `exercises_base` ADD `local_image_uri` text;--> statement-breakpoint
+ALTER TABLE `exercises_base` ADD `local_video_uri` text;--> statement-breakpoint
 ALTER TABLE `exercises_base` DROP COLUMN `video_public_id`;--> statement-breakpoint
 ALTER TABLE `exercises_base` DROP COLUMN `image_public_id`;
