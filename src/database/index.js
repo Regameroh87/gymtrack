903742e30c0cbc31e35e5e980a50cc2755947501
Regameroh87 <gamero.rodrigo@gmail.com>
2026-04-11 07:24:59 -0300

    refactor: remove useNewConnection option from sqlite database initialization

--- src/database/index.js
+++ src/database/index.js
@@ -4,9 +4,7 @@ import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 import * as schema from "./schemas";
 import migrations from "./migrations/migrations";
 
-const sqlite = openDatabaseSync("gymtrack.db", {
-  useNewConnection: true,
-});
+const sqlite = openDatabaseSync("gymtrack.db");
 
 export const database = drizzle(sqlite, { schema });
 
