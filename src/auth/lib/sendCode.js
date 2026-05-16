c1a50312986f3b73b1f53867538a18d83468af74
Regameroh87 <gamero.rodrigo@gmail.com>
2026-05-16 17:48:17 -0300

    refactor: add gym_id validation to verification email flow and improve error handling

--- src/auth/lib/sendCode.js
+++ src/auth/lib/sendCode.js
@@ -1,30 +1,34 @@
 import { supabase } from "../../database/supabase";
+
+const GYM_ID = process.env.EXPO_PUBLIC_GYM_ID;
+
 const sendCodeVerify = async (email) => {
   const { data, error } = await supabase
     .from("profiles")
-    .select("*")
-    .eq("email", email);
-  if (data) {
-    const { data: dataOtp, error: errorOtp } =
-      await supabase.auth.signInWithOtp({
-        email: email,
-        options: {
-          shouldCreateUser: false,
-        },
-      });
-    if (errorOtp) {
-      console.error("Error al enviar:", errorOtp.message);
-      throw errorOtp;
-    }
-    return dataOtp;
-  }
+    .select("id")
+    .eq("email", email)
+    .eq("gym_id", GYM_ID);
 
   if (error) {
     console.error("Error al enviar:", error.message);
     throw error;
   }
-  console.log(data);
-  return data;
+
+  if (!data || data.length === 0) {
+    throw new Error("Usuario no encontrado.");
+  }
+
+  const { data: dataOtp, error: errorOtp } = await supabase.auth.signInWithOtp({
+    email,
+    options: { shouldCreateUser: false },
+  });
+
+  if (errorOtp) {
+    console.error("Error al enviar:", errorOtp.message);
+    throw errorOtp;
+  }
+
+  return dataOtp;
 };
 
 export default sendCodeVerify;
