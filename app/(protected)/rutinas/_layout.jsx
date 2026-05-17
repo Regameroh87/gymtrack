d4aa324556909bbddb3b8e7ab38e6a298a6e56ed
Regameroh87 <gamero.rodrigo@gmail.com>
2026-05-17 19:22:42 -0300

    feat: implement training plan detail view with dynamic week and exercise breakdown

--- app/(protected)/rutinas/_layout.jsx
+++ app/(protected)/rutinas/_layout.jsx
@@ -24,6 +24,7 @@ export default function RutinasLayout() {
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="plan/[id]" options={{ headerShown: false }} />
     </Stack>
   );
 }
