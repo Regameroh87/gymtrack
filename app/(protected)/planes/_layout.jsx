38e1e91ec72ee69c749d87c668e434733c1dcd4a
Regameroh87 <gamero.rodrigo@gmail.com>
2026-05-29 07:25:40 -0300

    refactor: simplify planes layout to globally disable headers via Stack screenOptions

--- app/(protected)/planes/_layout.jsx
+++ app/(protected)/planes/_layout.jsx
@@ -1,31 +1,6 @@
 // Librerías externas
 import { Stack } from "expo-router";
-import { useColorScheme } from "nativewind";
-
-// Tema
-import { ui } from "../../../src/theme/colors";
 
 export default function PlanesLayout() {
-  const { colorScheme } = useColorScheme();
-  const isDark = colorScheme === "dark";
-
-  return (
-    <Stack
-      screenOptions={{
-        headerTitleStyle: {
-          fontFamily: "Lexend_700Bold",
-          color: isDark ? ui.text.mainDark : ui.text.main,
-        },
-        headerStyle: {
-          backgroundColor: isDark ? ui.background.dark : ui.background.light,
-        },
-        headerShadowVisible: false,
-        headerTintColor: isDark ? ui.text.mainDark : ui.text.main,
-      }}
-    >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="plan/[id]" options={{ headerShown: false }} />
-      <Stack.Screen name="builder" options={{ headerShown: false }} />
-    </Stack>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
