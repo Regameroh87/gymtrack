75c633268fee482244e55c3438ffc9fe693db0d0
Regameroh87 <gamero.rodrigo@gmail.com>
2026-06-05 17:41:52 -0300

    feat: add navigation stack layout for profile section with conditional header visibility

--- app/(protected)/profile/_layout.tsx
+++ app/(protected)/profile/_layout.tsx
@@ -1,7 +1,8 @@
 import { Stack } from "expo-router";
+import { Platform } from "react-native";
 export default function ProfileLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack screenOptions={{ headerShown: Platform.OS === "android" }}>
       <Stack.Screen name="index" />
     </Stack>
   );
