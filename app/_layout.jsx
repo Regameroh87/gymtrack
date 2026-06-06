50b836c8158ac52b694cfb4a1deacf5616910546
Regameroh87 <gamero.rodrigo@gmail.com>
2026-06-05 21:49:55 -0300

    feat: enable translucent status bar in root layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -161,6 +161,7 @@ export default function RootLayout() {
                 <BottomSheetModalProvider>
                   <StatusBar
                     style={colorScheme === "dark" ? "light" : "dark"}
+                    translucent
                   />
                   <Screen>
                     <Stack>
