34cc159378cacfc8e1dc731e37b955449cd3acb2
Regameroh87 <gamero.rodrigo@gmail.com>
2026-06-05 23:11:14 -0300

    fix: update navigation settings and header tint colors in planes layout

--- app/(protected)/(tabs)/planes/_layout.jsx
+++ app/(protected)/(tabs)/planes/_layout.jsx
@@ -6,17 +6,22 @@ import { Platform } from "react-native";
 import { ui } from "../../../../src/theme/colors";
 import { useTheme } from "../../../../src/theme/theme";
 
+export const unstable_settings = {
+  initialRouteName: "index",
+};
+
 export default function PlanesLayout() {
   const { isDark } = useTheme();
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
       <Stack.Screen
         name="builder"
         options={{
           headerShown: Platform.OS === "ios",
           headerTitle: "",
           headerBackButtonDisplayMode: "minimal",
-          headerTintColor: isDark ? ui.mutedDark : ui.mutedLigth,
+          headerTintColor: isDark ? ui.text.mainDark : ui.text.mutedDark,
         }}
       />
     </Stack>
