0de2b840c6a6ea21ca5de6a3eb064573b5c81062
Regameroh87 <gamero.rodrigo@gmail.com>
2026-06-29 11:09:11 -0300

    feat: implement environment-based app configuration for mobile build variants

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -12,14 +12,19 @@ const variants = {
 const variant = variants[APP_ENV];
 
 export default {
-  ...appJson.expo, // ← acá entra TODO tu app.json intacto
-  name: variant.name, // pisa solo el name
-  ios: {
-    ...appJson.expo.ios, // mantiene todo tu bloque ios
-    bundleIdentifier: variant.bundleId, // pisa solo el bundleIdentifier
-  },
-  android: {
-    ...appJson.expo.android, // mantiene todo tu bloque android
-    package: variant.bundleId, // pisa solo el package
+  expo: {
+    ...appJson.expo, // ← acá entra TODO tu app.json intacto
+    name: variant.name, // pisa solo el name
+    updates: {
+      url: "https://u.expo.dev/c5abcb20-7e97-4ed1-95b4-51dede8977c4",
+    },
+    ios: {
+      ...appJson.expo.ios, // mantiene todo tu bloque ios
+      bundleIdentifier: variant.bundleId, // pisa solo el bundleIdentifier
+    },
+    android: {
+      ...appJson.expo.android, // mantiene todo tu bloque android
+      package: variant.bundleId, // pisa solo el package
+    },
   },
 };
